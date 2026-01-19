45ca42836aafb6c36da56ab0806e4177de479a1a
valentin18-pt <valentinp33333@gmail.com>
2026-01-19 02:37:11 -0500

    Url backend

--- src/axios_services/apiConfig.js
+++ src/axios_services/apiConfig.js
@@ -1,4 +1,3 @@
-const API_URL_BASE = "http://127.0.0.1:8000/api/";
-// const API_URL_BASE = "https://app1.innovanetworking.com/api/";
+const API_URL_BASE = "https://nylah-oillike-avelina.ngrok-free.dev/api/";
 
 export default API_URL_BASE;
